eb0693cae1567f4a23c364215004ecb841066fbf
railsstudent <cleung334@gmail.com>
2020-08-12 09:34:38 +0800

    refactor:  Move types and interfaces to separate file

---
+++ types.ts
@@ -0,0 +1,85 @@
+export interface Planet {
+  name: string;
+}
+
+export interface Species {
+  name: string;
+  language: string;
+}
+
+export interface Film {
+  episodeId: number;
+  title: string; 
+}
+
+export enum Gender {
+  UNKNOWN = 'UNKNOWN',
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+  HERMAPHRODITE = 'HERMAPHRODITE',
+}
+
+export enum HairColor {
+  UNKNOWN = 'UNKNOWN',
+  BLONDE = 'BLONDE',
+  BROWN = 'BROWN',
+  BLACK = 'BLACK',
+  AUBURN = 'AUBURN',
+  WHITE = 'WHITE',
+  GREY = 'GREY',
+}
+
+export enum EyeColor {
+  UNKNOWN = 'UNKNOWN',
+  BLUE = 'BLUE',
+  YELLOW = 'YELLOW',
+  RED = 'RED',
+  BROWN = 'BROWN',
+  BLUEGREY = 'BLUEGREY',
+  BLACK = 'BLACK',
+  ORANGE = 'ORANGE',
+  HAZEL = 'HAZEL',
+  PINK = 'PINK',
+  GOLD = 'GOLD',
+  GREEN = 'GREEN',
+  WHITE = 'WHITE',
+  DARK = 'DARK'
+}
+
+export enum SkinColor {
+  UNKNOWN = 'UNKNOWN',
+  FAIR = 'FAIR',
+  GOLD = 'GOLD',
+  WHITE = 'WHITE',
+  LIGHT = 'LIGHT',
+  GREEN = 'GREEN',
+  GREENTAN = 'GREENTAN',
+  PALE = 'PALE',
+  METAL = 'METAL',
+  DARK = 'DARK',
+  BROWNMOTTLE = 'BROWNMOTTLE',
+  BROWN = 'BROWN',
+  GREY = 'GREY',
+  MOTTLEDGREEN = 'MOTTLEDGREEN',
+  ORANGE = 'ORANGE',
+  BLUE = 'BLUE',
+  RED = 'RED',
+  YELLOW = 'YELLOW',
+  TAN = 'TAN',
+  SILVER = 'SILVER'
+}
+
+export interface Person {
+  id: string;
+  name: string;
+  height: number;
+  mass: number;
+  birthYear: string;
+  gender: Gender;
+  eyeColor: EyeColor[];
+  hairColor: HairColor[];
+  skinColor: SkinColor[];
+  homeworld?: Planet;
+  species?: Species[];
+  films: Film[]
+}
